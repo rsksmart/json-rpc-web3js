8a3e15122a7b46bafa804b88429f8058514f4298
Juan Techera <juanjose@iovlabs.org>
2020-10-08 16:46:04 -0300

    Adding contract related tests (WIP)

--- ethereum-js-test.js
+++ ethereum-js-test.js
@@ -19,18 +19,18 @@ describe(`Rskj ethers.js Smoke Tests`, function () {
   var provider;
   let web3Provider;
   before(async () => {
-    
-      let url = "http://127.0.0.1:4444";
-      provider = new ethers.providers.JsonRpcProvider(url);
-      const signer = provider.getSigner();
-      web3 = new Web3('http://127.0.0.1:4444', null, {
-        transactionConfirmationBlocks: 1
-      });
-      web3.evm = {
-        mine: () => web3.currentProvider.send('evm_mine')
-      };
-     /* let currentProvider = new Web3.providers.HttpProvider('http://localhost:4444');
-      web3Provider = new ethers.providers.Web3Provider(currentProvider);*/    
+
+    let url = "http://127.0.0.1:4444";
+    provider = new ethers.providers.JsonRpcProvider(url);
+    const signer = provider.getSigner();
+    web3 = new Web3('http://127.0.0.1:4444', null, {
+      transactionConfirmationBlocks: 1
+    });
+    web3.evm = {
+      mine: () => web3.currentProvider.send('evm_mine')
+    };
+    /* let currentProvider = new Web3.providers.HttpProvider('http://localhost:4444');
+     web3Provider = new ethers.providers.Web3Provider(currentProvider);*/
   });
 
   it('Network should be RSK', async () => {
@@ -46,7 +46,7 @@ describe(`Rskj ethers.js Smoke Tests`, function () {
     for (let i = blockNumber; i < 5; i++) {
       await provider.send('evm_mine')
     }
-    blockNumber =  await provider.getBlockNumber();
+    blockNumber = await provider.getBlockNumber();
     assert.isAbove(blockNumber, 4);
   })
   it('Should have all the simple cached methods work', async () => {
@@ -72,7 +72,7 @@ describe(`Rskj ethers.js Smoke Tests`, function () {
 
     // net_peerCount
     let peerCount = await provider.send("net_peerCount");
-    assert(peerCount=='0x0', "Peer count is expected to be 0x0 but it is "+peerCount);
+    assert(peerCount == '0x0', "Peer count is expected to be 0x0 but it is " + peerCount);
 
 
     // net_version
@@ -99,8 +99,8 @@ describe(`Rskj ethers.js Smoke Tests`, function () {
     assert.isAbove(blockNumber, 4);
   });
 
-   // eth_gasPrice
-   it('eth_gasPrice: Should get the gas price', async () => {
+  // eth_gasPrice
+  it('eth_gasPrice: Should get the gas price', async () => {
     let gasPrice = await provider.getGasPrice();
     assert.equal(gasPrice.toNumber(), '0');
   });
@@ -127,7 +127,7 @@ describe(`Rskj ethers.js Smoke Tests`, function () {
       0
     );
     let expectedHistoricBalance = new BN(21000000000000000000000000);
-    assert.equal(historicBalance-expectedHistoricBalance, 0);
+    assert.equal(historicBalance - expectedHistoricBalance, 0);
 
     let unusedAccountBalance = await provider.getBalance(
       '0x09a1eda29f664ac8f68106f6567276df0c65d859',
@@ -140,9 +140,9 @@ describe(`Rskj ethers.js Smoke Tests`, function () {
   // eth_getTransactionByBlockNumberAndIndex
   it('eth_getTransactionByBlockHashAndIndex-eth_getTransactionByBlockNumberAndIndex: Should get transactions by block number and hash', async () => {
     let blockNumber = "0x1";
-    let byBlockNumber = await provider.send("eth_getTransactionByBlockNumberAndIndex", [blockNumber,"0x0"]);
+    let byBlockNumber = await provider.send("eth_getTransactionByBlockNumberAndIndex", [blockNumber, "0x0"]);
     let blockHash = byBlockNumber.blockHash;
-    let byHash = await provider.send("eth_getTransactionByBlockHashAndIndex", [blockHash,"0x0"]);
+    let byHash = await provider.send("eth_getTransactionByBlockHashAndIndex", [blockHash, "0x0"]);
     assert.deepEqual(byBlockNumber, byHash);
     let invalidBlock = await provider.send("eth_getTransactionByBlockNumberAndIndex", ['0xdeadbeef0fb9424aad2417321cac62915f6c83827f4d3c8c8c06900a61c4236c', 0]);
     assert.isNull(invalidBlock);
@@ -160,6 +160,87 @@ describe(`Rskj ethers.js Smoke Tests`, function () {
     assert.equal(byHash, expectedCount);
     assert.equal(byBlockNumber, expectedCount);
   });
+  it('eth_sendRawTransaction & eth_call: Should compile and deploy a contract successfully and interact with that contract', async function () {
+    this.timeout(20000);
+    let wallet = new ethers.Wallet(PRIVATE_KEY, provider);
+    let compiledHelloWorldPath = path.resolve(__dirname, 'Contracts', 'HelloWorld.json');
+    let compiledContract = fs.readFileSync(compiledHelloWorldPath, 'UTF-8');
+    let contractOutput = JSON.parse(compiledContract);
+    let abi = contractOutput.abi;
+    let bytecode = contractOutput.bytecode;
+    let factory = new ethers.ContractFactory(abi, bytecode, wallet);
+    let contract = await factory.deploy();
+   // await contract.deployed();
+    contractAddress = contract.address;
+    assert(contractAddress);
 
+    /*   let deployedContract = new web3.eth.Contract(abi, txReceipt.contractAddress);
+
+       let getCall = deployedContract.methods.get();
+       let callParams = {
+         to: txReceipt.contractAddress,
+         data: getCall.encodeABI(),
+       };
+
+       let currentVal = await web3.eth.call(callParams);
+       assert.equal(currentVal, '0x0000000000000000000000000000000000000000000000000000000000000005');
+
+       let currentValLatest = await web3.eth.call(callParams, "latest");
+       assert.equal(currentValLatest, '0x0000000000000000000000000000000000000000000000000000000000000005');
+
+       let currentValPending = await web3.eth.call(callParams, "pending");
+       assert.equal(currentValPending, '0x0000000000000000000000000000000000000000000000000000000000000005');
+
+       let setCall = deployedContract.methods.set(34);
+       let setGasEstimate = await setCall.estimateGas({ from: signedAccount.address });
+       let transactionParameters = {
+         to: txReceipt.contractAddress,
+         from: signedAccount.address,
+         gasPrice: '0x4A817C800', // 20000000000
+         gas: setGasEstimate,
+         data: setCall.encodeABI(),
+         chainId: 33
+       };
+
+       let setSignedTx = await signedAccount.signTransaction(transactionParameters);
+
+       // Send the transaction.
+       let receipt = await web3.eth.sendSignedTransaction(setSignedTx.rawTransaction)
+         .once('transactionHash', (hash) => {
+           assert.isString(hash);
+           trxHash = hash;
+         })
+         .on('error', (error) => {
+           assert(false, `Unexpected error sending set transaction: $`);
+         });
+         
+       let receiptString = JSON.stringify(receipt);
+       assert(receiptString.indexOf('transactionHash') > 0, "transactionHash is not being returned and it's expected!");
+       assert(receiptString.indexOf('transactionIndex') > 0, "transactionIndex is not being returned and it's expected!");
+       assert(receiptString.indexOf('blockHash') > 0, "blockHash is not being returned and it's expected!");
+       assert(receiptString.indexOf('blockNumber') > 0, "blockNumber is not being returned and it's expected!");
+       assert(receiptString.indexOf('cumulativeGasUsed') > 0, "cumulativeGasUsed is not being returned and it's expected!");
+       assert(receiptString.indexOf('gasUsed') > 0, "gasUsed is not being returned and it's expected!");
+       assert(receiptString.indexOf('contractAddress') > 0, "contractAddress is not being returned and it's expected!");
+       assert(receiptString.indexOf('logs') > 0, "logs is not being returned and it's expected!");
+       assert(receiptString.indexOf('from') > 0, "from is not being returned and it's expected!");
+       assert(receiptString.indexOf('to') > 0, "to is not being returned and it's expected!");
+       assert(receiptString.indexOf('root') > 0, "root is not being returned and it's expected!");
+       assert(receiptString.indexOf('status') >0, "status is not being returned and it's expected!");
+       assert(receiptString.indexOf('logsBloom') > 0, "logsBloom is not being returned and it's expected!");
+
+       await new Promise((res) => setTimeout(res, 5000));
+
+       await deployedContract.getPastEvents('ValueChanged', { fromBlock: 0, toBlock: 'latest' }, (error, eventLogs) => {
+         assert(!error, `Unexpected error reading logs ${error}`);
+         assert.equal(eventLogs[0].returnValues.newValue, "34");
+       });*/
+  });
+
+  //eth_coinbase
+  it(`eth_coinbase: Should return coinbase from RskJ`, async () => {
+    let coinbase = await provider.send("eth_coinbase");
+    assert.equal(coinbase, '0xec4ddeb4380ad69b3e509baad9f158cdf4e4681d');
+  });
 
 });
